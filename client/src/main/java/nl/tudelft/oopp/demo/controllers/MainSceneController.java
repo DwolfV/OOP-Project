53162d0ebb3a272be174b3623b75fa879bcd8574
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-02-24 12:17:58 +0100

    JavaFX changes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -1,6 +1,7 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 
 public class MainSceneController {
@@ -8,11 +9,18 @@ public class MainSceneController {
     /**
      * Handles clicking the button.
      */
-    public void buttonClicked() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Quote for you");
-        alert.setHeaderText(null);
-        alert.setContentText(ServerCommunication.getQuote());
-        alert.showAndWait();
+
+    public Button button;
+
+    public void logInButtonClicked() {
+        System.out.println("User logged in");
+
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle("Quote for you");
+//        alert.setHeaderText(null);
+//        alert.setContentText(ServerCommunication.getQuote());
+//        alert.showAndWait();
+
+
     }
 }
