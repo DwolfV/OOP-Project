3a70404ec8344bb3eed03fc7da209f8bde9a8ea7
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-02-25 15:19:16 +0100

    Changed some stuff that sourcetree added.

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,19 +1,13 @@
 package nl.tudelft.oopp.demo;
 
-<<<<<<< HEAD
 import nl.tudelft.oopp.demo.views.MainDisplay;
-=======
 import nl.tudelft.oopp.demo.views.BuildingsDisplay;
->>>>>>> master
 
 public class MainApp {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         MainDisplay.main(new String[0]);
-=======
         BuildingsDisplay.main(new String[0]);
->>>>>>> master
     }
 
 }
