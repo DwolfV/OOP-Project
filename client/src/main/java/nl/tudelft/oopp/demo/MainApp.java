f95682129b96ca36e970c989bf44e8e48da2be50
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-02-25 21:44:37 +0100

    I removed BuildingsDisplay.java since it's the same as my MainDisplay.java. Also, I added some FXML code to the friendsScene.fxml like adding a button to allow the user to add friends, and also a button/label to remove friends. For now I hardcoded it but later on we will connect it to the database.

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,13 +1,11 @@
 package nl.tudelft.oopp.demo;
 
 import nl.tudelft.oopp.demo.views.MainDisplay;
-import nl.tudelft.oopp.demo.views.BuildingsDisplay;
 
 public class MainApp {
 
     public static void main(String[] args) {
         MainDisplay.main(new String[0]);
-        BuildingsDisplay.main(new String[0]);
     }
 
 }
