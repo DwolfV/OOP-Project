270b4aaaaa2898b9a921a15a4642515aa1891101
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-02-29 19:50:56 +0100

    Changed the add scenes.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
@@ -3,6 +3,7 @@ package nl.tudelft.oopp.demo.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,7 +13,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -73,91 +76,100 @@ public class AdminSceneController implements Initializable {
         Stage secondStage = new Stage();
         Scene scene = new Scene(new Group());
 
-        secondStage.setTitle("Table View Sample");
+        secondStage.setTitle("Table View");
         secondStage.setWidth(450);
         secondStage.setHeight(550);
 
-        final Label label = new Label("Address Book");
+        final Label label = new Label("Buildings");
         label.setFont(new Font("Arial", 20));
 
         table.setEditable(true);
 
-        TableColumn firstNameCol = new TableColumn("id");
-        firstNameCol.setMinWidth(100);
-        firstNameCol.setCellValueFactory(
+        TableColumn<Building, String> idCol =
+                new TableColumn<>("id");
+        idCol.setMinWidth(100);
+        idCol.setCellValueFactory(
                 new PropertyValueFactory<>("id"));
+//        idCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+//        idCol.setOnEditCommit(
+//                (TableColumn.CellEditEvent<Building, String> t) -> {
+//                    ((Building) t.getTableView().getItems().get(
+//                            t.getTablePosition().getRow())
+//                    ).setId(t.getNewValue());
+//                });
 
-        TableColumn lastNameCol = new TableColumn("name");
-        lastNameCol.setMinWidth(100);
-        lastNameCol.setCellValueFactory(
+        TableColumn<Building, String> buildingCol =
+                new TableColumn<>("Building Name");
+        buildingCol.setMinWidth(100);
+        buildingCol.setCellValueFactory(
                 new PropertyValueFactory<>("name"));
+        buildingCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+        buildingCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<Building, String> t) -> {
+                    ((Building) t.getTableView().getItems().get(
+                            t.getTablePosition().getRow())
+                    ).setId(t.getNewValue());
+                });
 
-        TableColumn emailCol = new TableColumn("streetName");
-        emailCol.setMinWidth(200);
-        emailCol.setCellValueFactory(
+        TableColumn<Building, String> streetNameCol =
+                new TableColumn<>("Street Name");
+        streetNameCol.setMinWidth(100);
+        streetNameCol.setCellValueFactory(
                 new PropertyValueFactory<>("streetName"));
+        streetNameCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+        streetNameCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<Building, String> t) -> {
+                    ((Building) t.getTableView().getItems().get(
+                            t.getTablePosition().getRow())
+                    ).setId(t.getNewValue());
+                });
 
-        TableColumn CityCol = new TableColumn("streetNumber");
-        CityCol.setMinWidth(200);
-        CityCol.setCellValueFactory(
+        TableColumn<Building, String> streetNumCol =
+                new TableColumn<>("Street Number");
+        streetNumCol.setMinWidth(100);
+        streetNumCol.setCellValueFactory(
                 new PropertyValueFactory<>("streetNumber"));
+        streetNumCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+        streetNumCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<Building, String> t) -> {
+                    ((Building) t.getTableView().getItems().get(
+                            t.getTablePosition().getRow())
+                    ).setId(t.getNewValue());
+                });
 
-        TableColumn ZipCol = new TableColumn("zipCode");
-        ZipCol.setMinWidth(200);
-        ZipCol.setCellValueFactory(
+        TableColumn<Building, String> zipCodeCol =
+                new TableColumn<>("Zip Code");
+        zipCodeCol.setMinWidth(100);
+        zipCodeCol.setCellValueFactory(
                 new PropertyValueFactory<>("zipCode"));
+        zipCodeCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+        zipCodeCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<Building, String> t) -> {
+                    ((Building) t.getTableView().getItems().get(
+                            t.getTablePosition().getRow())
+                    ).setId(t.getNewValue());
+                });
 
-        TableColumn StreetCol = new TableColumn("city");
-        StreetCol.setMinWidth(200);
-        StreetCol.setCellValueFactory(
+        TableColumn<Building, String> cityCol =
+                new TableColumn<>("City");
+        cityCol.setMinWidth(100);
+        cityCol.setCellValueFactory(
                 new PropertyValueFactory<>("city"));
+        cityCol.setCellFactory(TextFieldTableCell.<Building>forTableColumn());
+        cityCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<Building, String> t) -> {
+                    ((Building) t.getTableView().getItems().get(
+                            t.getTablePosition().getRow())
+                    ).setId(t.getNewValue());
+                });
 
         table.setItems(data);
-        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol, CityCol, ZipCol, StreetCol);
-
-        final TextField addIdName = new TextField();
-        addIdName.setPromptText("id Name");
-        addIdName.setMaxWidth(firstNameCol.getPrefWidth());
-        final TextField addLastName = new TextField();
-        addLastName.setMaxWidth(lastNameCol.getPrefWidth());
-        addLastName.setPromptText("Last Name");
-        final TextField addEmail = new TextField();
-        addEmail.setMaxWidth(emailCol.getPrefWidth());
-        addEmail.setPromptText("Email");
-        final TextField addCity = new TextField();
-        addCity.setMaxWidth(emailCol.getPrefWidth());
-        addCity.setPromptText("Email");
-        final TextField addZip = new TextField();
-        addZip.setMaxWidth(emailCol.getPrefWidth());
-        addZip.setPromptText("Email");
-        final TextField addStreet = new TextField();
-        addStreet.setMaxWidth(emailCol.getPrefWidth());
-        addStreet.setPromptText("Email");
-
-        final Button addButton = new Button("Add");
-        addButton.setOnAction((ActionEvent e) -> {
-            data.add(new Building(
-                    addIdName.getId(),
-                    addLastName.getText(),
-                    addEmail.getText(),
-                    addCity.getText(),
-                    addZip.getText(),
-                    addStreet.getText()));
-            addIdName.clear();
-            addLastName.clear();
-            addEmail.clear();
-            addCity.clear();
-            addZip.clear();
-            addStreet.clear();
-        });
-
-        hb.getChildren().addAll(addIdName, addLastName, addEmail, addCity, addZip, addStreet, addButton);
-        hb.setSpacing(3);
+        table.getColumns().addAll(idCol, buildingCol, streetNameCol, streetNumCol, zipCodeCol, cityCol);
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, table, hb);
+        vbox.getChildren().addAll(label, table);
 
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
