c0f74b3ec6e3017d4b37009ada029e46f32b768d
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-03-03 15:41:51 +0100

    On the GUI side, when the user wants to log in they now will have the option to sign up, which will take them to a new scene. Also, I added a hyperlink for forgetting the password, this will be later implemented, so for now it doesn't do anything.

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterSceneController.java
@@ -0,0 +1,15 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterSceneController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+
+}
