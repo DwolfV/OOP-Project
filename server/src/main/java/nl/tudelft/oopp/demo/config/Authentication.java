1eb7e868fb56623faa4dd0ad7c2e5c0d92fd6724
Atanas Pashov <a.i.pashov@student.tudelft.nl>
2020-03-03 22:43:43 +0100

    Added a new @Bean in the Authentication class that will deal with user creation and checking if users exist in the database.

--- server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
@@ -1,19 +1,22 @@
 package nl.tudelft.oopp.demo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class Authentication extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -22,33 +25,18 @@ public class Authentication extends WebSecurityConfigurerAdapter {
     // add two demo users
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("{noop}password").roles("USER")
-//                .and()
-//                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
         auth.jdbcAuthentication()
                 .dataSource(dataSource);
-                //.withDefaultSchema()
-//                .withUser(User.withUsername("user")
-//                        .password(passwordEncoder().encode("pass"))
-//                        .roles("USER"));
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
-    // Secure the endpoins with HTTP Basic authentication
+    // Secure the endpoints with HTTP Basic authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 //HTTP Basic authentication
                 .authorizeRequests()
-//                .httpBasic()
-//                .and()
-//                .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/rooms/**").hasRole("USER")
                 .antMatchers(HttpMethod.POST, "/rooms").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/rooms/**").hasRole("ADMIN")
@@ -58,7 +46,26 @@ public class Authentication extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
                 .and()
-                .csrf().disable()       // TODO think about csfr? + HTTPS?
+                .csrf().disable()
                 .formLogin().disable();
+    }
+
+    /**
+     * Create a new Bean for JDBC user detail management.
+     * It is used for creating users and checking if users exist.
+     *
+     * @return A new JdbcUserDetailsManager
+     * */
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(dataSource);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
