3e418ff6b263970e5eaf2b668aa67706250964a2
snutzel <s.d.nutzel@student.tudelft.nl>
2020-03-04 14:48:45 +0100

    Eddited both Repository test (Room and User) to work well and have no checkstyle errors. Also added a Controller test (User) that works and has no checkstyle errors

--- server/src/test/java/nl/tudelft/oopp/demo/repositories/RoomRepositoryTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/repositories/RoomRepositoryTest.java
@@ -1,5 +1,13 @@
 package nl.tudelft.oopp.demo.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.entities.Room;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,20 +15,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 @DataJpaTest
 public class RoomRepositoryTest {
 
-    List<Room> r1;
-    List<Room> r2;
-    List<Room> r3;
-    List<Room> r4;
-    List<Room> r5;
+    private List<Room> r1;
+    private List<Room> r2;
+    private List<Room> r3;
+    private List<Room> r4;
+    private List<Room> r5;
 
     @Autowired
     private RoomRepository roomRep;
@@ -28,53 +30,104 @@ public class RoomRepositoryTest {
     @Autowired
     private BuildingRepository buildRep;
 
+    /**
+     * Creates all rooms and is done before each test.
+     */
     @BeforeEach
     public void save() {
-        r1 = new ArrayList<Room>();
-        r2 = new ArrayList<Room>();
-        r3 = new ArrayList<Room>();
-        r4 = new ArrayList<Room>();
-        r5 = new ArrayList<Room>();
         Building b1 = new Building(1, "b1", "s1", "sNo1", "z1", "c1");
-        r1.add(new Room(1, "room1", 1, b1));
-        r1.add(new Room(2, "room2", 2, b1));
-        r1.add(new Room(3, "room3", 3, b1));
+        r1 = new ArrayList<Room>(List.of(new Room(1, "room1", 1, b1)));
+        r2 = new ArrayList<Room>(List.of(new Room(2, "room2", 2, b1)));
+        r3 = new ArrayList<Room>(List.of(new Room(3, "room3", 3, b1)));
         Building b2 = new Building(2, "b2", "s2", "sNo2", "z2", "c2");
-        r1.add(new Room(4, "room4", 4, b2));
-        r1.add(new Room(5, "room5", 5, b2));
+        r4 = new ArrayList<Room>(List.of(new Room(4, "room4", 4, b2)));
+        r5 = new ArrayList<Room>(List.of(new Room(5, "room5", 5, b2)));
         buildRep.save(b1);
         buildRep.save(b2);
-        roomRep.save(r1.get(0));
-        roomRep.save(r2.get(0));
-        roomRep.save(r3.get(0));
-        roomRep.save(r4.get(0));
-        roomRep.save(r5.get(0));
+        roomRep.saveAll(r1);
+        roomRep.saveAll(r2);
+        roomRep.saveAll(r3);
+        roomRep.saveAll(r4);
+        roomRep.saveAll(r5);
     }
 
+    /**
+     * Checks whether findAll() gets a list of all the rooms.
+     */
     @Test
-    public void findRoomsByBuildingName() {
+    public void getAllRooms() {
+        List<Room> fullList = new ArrayList<Room>();
+        fullList.addAll(r1);
+        fullList.addAll(r2);
+        fullList.addAll(r3);
+        fullList.addAll(r4);
+        fullList.addAll(r5);
+        assertEquals(fullList, roomRep.findAll());
+    }
+
+    /**
+     * Checks whether findRoomsByBuildingId() in the repository
+     * finds all the rooms from a certain building.
+     */
+    @Test
+    public void getRoomsByBuildingId() {
         List<Room> l1 = new ArrayList<Room>();
         l1.addAll(r1);
         l1.addAll(r2);
         l1.addAll(r3);
+        List<Room> el1 = roomRep.findByBuildingId(1);
         List<Room> l2 = new ArrayList<Room>();
-        l1.addAll(r4);
-        l1.addAll(r5);
-        assertEquals(l1, roomRep.findByBuildingId(1));
-        assertEquals(l2, roomRep.findByBuildingId(2));
+        l2.addAll(r4);
+        l2.addAll(r5);
+        List<Room> el2 = roomRep.findByBuildingId(2);
+        assertEquals(l1.size(), el1.size());
+        assertEquals(l2.size(), el2.size());
     }
 
+    /**
+     * Checks whether findRoomsByBuildingId() in the repository
+     * finds all the rooms from a certain building.
+     */
     @Test
-    public void findRoomByName() {
-        assertEquals(r1, roomRep.findByName("room1"));
-        assertEquals(r2, roomRep.findByName("room2"));
-        assertEquals(r3, roomRep.findByName("room3"));
-        assertEquals(r4, roomRep.findByName("room4"));
-        assertEquals(r5, roomRep.findByName("room5"));
-        assertEquals(null, roomRep.findByName("non_existent"));
-        assertNotEquals(r1, roomRep.findByName("room5"));
-        assertNotEquals(r2, roomRep.findByName("room3"));
-        assertNotEquals(r2, null);
+    public void getRoomsByBuildingIdCheckAll() {
+        List<Room> totalList = roomRep.findAll();
+        List<Room> b1List = roomRep.findByBuildingId(1);
+        List<Room> b2List = roomRep.findByBuildingId(2);
+        for (int i = 0; i < totalList.size(); i++) {
+            Room tempRoom = totalList.get(1);
+            if (tempRoom.getBuilding().getId() == 1) {
+                assertTrue(b1List.contains(tempRoom));
+            } else if (tempRoom.getBuilding().getId() == 2) {
+                assertTrue(b2List.contains(tempRoom));
+            } else {
+                fail("Building should not exist");
+            }
+        }
+    }
+
+    /**
+     * Checks whether findByName() actually returns the room(s) with that name.
+     */
+    @Test
+    public void getRoomByName() {
+        List<Room> fullList = roomRep.findAll();
+        assertTrue(fullList.size() == 5);          // Make sure fullList has 5 objects
+        List<Room> room1 = new ArrayList<Room>(List.of(fullList.get(0)));
+        assertEquals(room1, roomRep.findByName("room1"));
+        List<Room> room2 = new ArrayList<Room>(List.of(fullList.get(1)));
+        assertEquals(room2, roomRep.findByName("room2"));
+        List<Room> room3 = new ArrayList<Room>(List.of(fullList.get(2)));
+        assertEquals(room3, roomRep.findByName("room3"));
+        List<Room> room4 = new ArrayList<Room>(List.of(fullList.get(3)));
+        assertEquals(room4, roomRep.findByName("room4"));
+        List<Room> room5 = new ArrayList<Room>(List.of(fullList.get(4)));
+        assertEquals(room5, roomRep.findByName("room5"));
+
+        List<Room> emptyList = new ArrayList<Room>();
+        assertEquals(emptyList, roomRep.findByName("non_existent"));
+        assertNotEquals(room1, roomRep.findByName("room5"));
+        assertNotEquals(room2, roomRep.findByName("room3"));
+        assertNotEquals(room2, emptyList);
     }
 
 }
--- server/src/test/java/nl/tudelft/oopp/demo/repositories/UserRepositoryTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/repositories/UserRepositoryTest.java
@@ -1,65 +1,76 @@
 package nl.tudelft.oopp.demo.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import nl.tudelft.oopp.demo.entities.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 @DataJpaTest
 public class UserRepositoryTest {
 
-    List<User> u1;
-    List<User> u2;
-    List<User> u3;
-    List<User> u4;
-    List<User> u5;
+    private List<User> u1;
+    private List<User> u2;
+    private List<User> u3;
+    private List<User> u4;
+    private List<User> u5;
 
     @Autowired
     private UserRepository userRep;
 
+    /**
+     * Creates all users and is done before each test.
+     */
     @BeforeEach
-    private void save() {
-        u1 = new ArrayList<User>(List.of(new User(1, "user1@email.com", "student", "fn2", "ln1", new Date(1000))));
-        u2 = new ArrayList<User>(List.of(new User(2, "user2@email.com", "student", "fn3", "ln2", new Date(2000))));
-        u3 = new ArrayList<User>(List.of(new User(3, "user3@email.com", "student", "fn4", "ln3", new Date(3000))));
-        u4 = new ArrayList<User>(List.of(new User(4, "user4@email.com", "employee", "fn5", "ln4", new Date(4000))));
-        u5 = new ArrayList<User>(List.of(new User(5, "user5@email.com", "employee", "fn6", "ln5", new Date(5000))));
+    public void save() {
+        u1 = new ArrayList<User>(List.of(
+                new User(1, "user1@email.com", "student", "fn1", "ln1", new Date(1000))));
+        u2 = new ArrayList<User>(List.of(
+                new User(2, "user2@email.com", "student", "fn2", "ln2", new Date(2000))));
+        u3 = new ArrayList<User>(List.of(
+                new User(3, "user3@email.com", "student", "fn3", "ln3", new Date(3000))));
+        u4 = new ArrayList<User>(List.of(
+                new User(4, "user4@email.com", "employee", "fn4", "ln4", new Date(4000))));
+        u5 = new ArrayList<User>(List.of(
+                new User(5, "user5@email.com", "employee", "fn5", "ln5", new Date(5000))));
         userRep.saveAll(u1);
-        userRep.saveAll(u1);
-        userRep.saveAll(u1);
-        userRep.saveAll(u1);
-        userRep.saveAll(u1);
+        userRep.saveAll(u2);
+        userRep.saveAll(u3);
+        userRep.saveAll(u4);
+        userRep.saveAll(u5);
     }
 
     @Test
-    private void getUserByEmail() {
+    public void getUserByEmail() {
         assertEquals(u1, userRep.findByEmail("user1@email.com"));
         assertEquals(u2, userRep.findByEmail("user2@email.com"));
         assertEquals(u3, userRep.findByEmail("user3@email.com"));
         assertEquals(u4, userRep.findByEmail("user4@email.com"));
         assertEquals(u5, userRep.findByEmail("user5@email.com"));
-        assertEquals(null, userRep.findByEmail("invalid_email"));
+        List<User> emptyList = new ArrayList<User>();
+        assertEquals(emptyList, userRep.findByEmail("invalid_email"));
+        assertNotEquals(null, userRep.findByEmail("invalid_email"));
+        assertNotEquals(emptyList, userRep.findByEmail("user1@email.com"));
         assertNotEquals(null, userRep.findByEmail("user1@email.com"));
     }
 
     @Test
-    private void findUserByRole() {
-        List<User> sList = new ArrayList<User>();
-        sList.addAll(u1);
-        sList.addAll(u2);
-        sList.addAll(u3);
-        List<User> eList = new ArrayList<User>();
-        eList.addAll(u4);
-        eList.addAll(u5);
-        assertEquals(sList, userRep.findByRole("student"));
-        assertEquals(eList, userRep.findByRole("employee"));
+    public void findUserByRole() {
+        List<User> slist = new ArrayList<User>();
+        slist.addAll(u1);
+        slist.addAll(u2);
+        slist.addAll(u3);
+        List<User> elist = new ArrayList<User>();
+        elist.addAll(u4);
+        elist.addAll(u5);
+        assertEquals(slist, userRep.findByRole("student"));
+        assertEquals(elist, userRep.findByRole("employee"));
     }
 }
