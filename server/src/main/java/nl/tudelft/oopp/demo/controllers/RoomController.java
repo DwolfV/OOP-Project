475e05797baf5bc4a8f33c8ebe35b587f159ac13
Iulia Dinu <i.dinu-1@student.tudelft.nl>
2020-03-05 01:38:37 +0100

    modified RoomController class

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
@@ -1,22 +1,23 @@
 package nl.tudelft.oopp.demo.controllers;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+import javax.validation.Valid;
 import nl.tudelft.oopp.demo.entities.Room;
-import nl.tudelft.oopp.demo.repositories.BuildingRepository;
 import nl.tudelft.oopp.demo.repositories.RoomRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.hateoas.EntityModel;
-//import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 public class RoomController {
