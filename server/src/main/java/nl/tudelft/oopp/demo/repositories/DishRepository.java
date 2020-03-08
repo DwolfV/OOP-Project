9baa8f2e176ac35a4aab614c717a5534206784f3
Iulia Dinu <i.dinu-1@student.tudelft.nl>
2020-03-08 21:16:33 +0100

    implemented DishRepository

---
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/DishRepository.java
@@ -0,0 +1,11 @@
+package nl.tudelft.oopp.demo.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import nl.tudelft.oopp.demo.entities.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DishRepository extends JpaRepository<Dish, Long> {
+    Optional<Dish>  findById(long id);
+    List<Dish> findByName(String name);
+}
