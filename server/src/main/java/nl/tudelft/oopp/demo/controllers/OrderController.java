cc2d25e4d134fcba19509a1e8fb4f00872f90ed3
Andrei <A.Mija@student.tudelft.nl>
2020-03-12 15:42:28 +0100

    Modified some classes to check for authentication and created tests from the event and order repositories and controllers
    - OrderController now checks if teh user that tries to access the order is actually the owner of the order
    - Eliminated NotBlank atnotations because they were not working for some attributes
    - Tested the repositories and controllers for the Order and Event entities

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/OrderController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/OrderController.java
@@ -1,10 +1,13 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import nl.tudelft.oopp.demo.entities.Order;
+import nl.tudelft.oopp.demo.entities.RoomReservation;
 import nl.tudelft.oopp.demo.repositories.OrderRepository;
+import nl.tudelft.oopp.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,38 +31,95 @@ public class OrderController {
     @Autowired
     OrderRepository repository;
 
+    @Autowired
+    UserRepository users;
+
     @GetMapping("/all")
     public List<Order> getAllOrders() {
         return repository.findAll();
     }
 
     @GetMapping("/room_reservation/{id}")
-    public List<Order> getOrdersByReservation(@PathVariable long id) {
-        return repository.findOrdersByRoomReservationId();
+    public List<Order> getOrdersByReservationId(@PathVariable long id) {
+        return repository.findOrdersByRoomReservationId(id);
     }
 
-    @PostMapping(value = "/add", consumes = "application/json")
-    public ResponseEntity<Order> addOrder(@Valid @RequestBody Order order, UriComponentsBuilder o) {
-        repository.save(order);
-        UriComponents uri = o.path("/order/{id}").buildAndExpand(order.getId());
-        return ResponseEntity.created(uri.toUri()).body(order);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Order> updateOrder(@PathVariable long id, @RequestBody Order newOrder) {
-        return repository.findById(id).map(order -> {
-            order.setDishOrders(newOrder.getDishOrders());
-            order.setRoomReservation(newOrder.getRoomReservation());
-
-            return new ResponseEntity<>(repository.save(order), HttpStatus.OK);
+    /**
+     * Gets all the events linked to a user
+     * @param id - the id of the user by which the orders are retrieved
+     * @param authentication - parameter used to check if the current user has the same id as the id of the user that owns the orders
+     * @return a List of Orders which are linked to the logged in user
+     */
+    @GetMapping("/user/{id}")
+    public @ResponseBody ResponseEntity<List<Order>> getOrdersByUserId(@PathVariable long id, Authentication authentication) {
+        System.out.println(authentication.getName());
+        return users.findByUsername(authentication.getName()).map(user -> {
+            if (user.getId() == id) return new ResponseEntity<>(repository.findAllByRoomReservation_User_Id(id), HttpStatus.OK);
+            return new ResponseEntity<List<Order>>(HttpStatus.UNAUTHORIZED);
         }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+//    @PostMapping(value = "/add", consumes = "application/json")
+//    public ResponseEntity<Order> addOrder(@Valid @RequestBody Order order, UriComponentsBuilder o) {
+//        repository.save(order);
+//        UriComponents uri = o.path("/order/{id}").buildAndExpand(order.getId());
+//        return ResponseEntity.created(uri.toUri()).body(order);
+//    }
+
+    @PostMapping(value="/add", consumes = {"application/json"})
+    public ResponseEntity<Order> addOrder(@Valid @RequestBody Order newOrder, UriComponentsBuilder b, Authentication authentication) {
+
+        if (authentication.getName().equals(newOrder.getRoomReservation().getUser().getUsername())
+                && !users.findByUsername(authentication.getName()).isEmpty()
+                && newOrder.getRoomReservation().getUser().getId() == users.findByUsername(authentication.getName()).get().getId()) {
+
+            Order savedOrder = repository.save(newOrder);
+            UriComponents uri = b.path("/order/{id}").buildAndExpand(savedOrder.getId());
+            return ResponseEntity
+                    .created(uri.toUri())
+                    .body(savedOrder);
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable long id, @RequestBody Order newOrder, UriComponentsBuilder b, Authentication authentication) {
+
+        UriComponents uri = b.path("order/{id}").buildAndExpand(id);
+
+        return repository.findById(id).map(order -> {
+            if (users.findByUsername(authentication.getName()).isEmpty() || order.getRoomReservation().getUser().getId() != users.findByUsername(authentication.getName()).get().getId()
+                    || order.getRoomReservation().getUser().getId() != users.findByUsername(authentication.getName()).get().getId())
+                return new ResponseEntity<Order>(HttpStatus.UNAUTHORIZED);
+
+            order.setDishOrders(newOrder.getDishOrders());
+            order.setRoomReservation(newOrder.getRoomReservation());
+
+            return ResponseEntity.created(uri.toUri()).body(repository.save(order));
+        }).orElseGet(() -> {newOrder.setId(id);
+                    return ResponseEntity.created(uri.toUri()).body(repository.save(newOrder));
+        });
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteOrder(@PathVariable long id) {
-        return repository.findById(id).map( order -> {
-            repository.delete(order);
-            return new ResponseEntity("The order has been deleted successfully", HttpStatus.OK);
-        }).orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    public ResponseEntity<?> deleteOrder(@PathVariable long id, Authentication authentication) {
+//        return repository.findById(id).map( order -> {
+//            repository.delete(order);
+//            return new ResponseEntity("The order has been deleted successfully", HttpStatus.OK);
+//        }).orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+
+        Order orderToDelete = repository.findById(id).orElseGet(() -> null);
+
+        if (orderToDelete == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        if (!orderToDelete.getRoomReservation().getUser().getUsername().equals(authentication.getName())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
     }
 }
--- server/src/main/java/nl/tudelft/oopp/demo/repositories/OrderRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/OrderRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    public List<Order> findOrdersByRoomReservationId();
+    public List<Order> findOrdersByRoomReservationId(long id);
+
+    public List<Order> findAllByRoomReservation_User_Id(long id);
 }
