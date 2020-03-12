f4f34af5ae45b1a231b86aad0e07c8aebcf26c4c
Iulia Dinu <i.dinu-1@student.tudelft.nl>
2020-03-13 00:00:23 +0100

    modified method findByName in repository for the entity Restaurant

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/RestaurantRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/RestaurantRepository.java
@@ -2,12 +2,8 @@ package nl.tudelft.oopp.demo.repositories;
 
 import java.util.List;
 import java.util.Optional;
-
-import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.entities.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
@@ -21,7 +17,7 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
      */
     List<Restaurant> findAllByBuildingName(String buildingName);
 
-    List<Restaurant> findByName(String name);
+    Optional<Restaurant> findByName(String name);
 
     Optional<Restaurant> findById(long id);
 }
