d511a6ab7ef26e8d9fe4fb3cd18d42da69352fe2
Atanas Pashov <a.i.pashov@student.tudelft.nl>
2020-03-16 15:39:29 +0100

    Added several checks that allow the user to access/modify/create orders.
    We still need to implement checks that ensure that the user can only order when he is in a Room in the same Building as the Restaurant.

--- server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
@@ -39,13 +39,16 @@ public class Authentication extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/building/**", "/equipment/**", "/holidays/**",
                         "/openTimes/**", "/restaurant/**", "/rooms/**",
-                        "/room_reservations/**", "/supply/**", "/login", "/room_reservations_times/**")
+                        "/room_reservations/**", "/supply/**", "/login", "/room_reservations_times/**", "/order/**")
                         .hasAnyRole("USER", "ADMIN")
                 .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET, "/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/room_reservations/**").hasRole("USER")
                 .antMatchers(HttpMethod.PUT, "/room_reservations/**").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE, "/room_reservations/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/order/**").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/order/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/order/**").hasRole("USER")
                 .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
