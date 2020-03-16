b3f5298614d5272e1d36e84ed1ce5913e3ef2091
Iulia Dinu <i.dinu-1@student.tudelft.nl>
2020-03-16 13:11:13 +0100

    fixed the checkstyle errors in the Dish entity

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Dish.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Dish.java
@@ -1,8 +1,15 @@
 package nl.tudelft.oopp.demo.entities;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "Dish")
