d1424dbee85018e8962d6d7684d242ab82c834f7
Andrei <A.Mija@student.tudelft.nl>
2020-03-18 21:13:21 +0100

    Modified the controller and the server communication class for the OpenTime entity
    - Commented out the method which would get an OpenTime by id from the controller because it was implemented wrong and not needed at the moment
    - Fixed an miss-spelled url to get all the open times in the server communication class
    - Added a new method in the server communication class to get OpenTimes by building id

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/OpenTimeController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/OpenTimeController.java
@@ -43,7 +43,7 @@ public class OpenTimeController {
      * @return a list of the openTimes for the building {@link OpenTime}.
      */
 
-    @GetMapping("openTimes/{building_id}")
+    @GetMapping("openTimes/building/{building_id}")
     public @ResponseBody ResponseEntity<List<OpenTime>>
         getOpenTimesForBuilding(@PathVariable(value = "building_id") long id /*,
                                                    @RequestParam String parameter*/) {
@@ -65,19 +65,19 @@ public class OpenTimeController {
         ).orElseGet( () -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    /**
-     * GET Endpoint to retrieve holiday by ID.
-     *
-     * @param openTime_id Unique identifier of the equipment.
-     * @return The requested equipment {@link OpenTime}.
-     */
-
-    @GetMapping(value = "/openTimes", consumes = {"application/json"})
-    public @ResponseBody ResponseEntity<OpenTime> getOpenTimeById(@PathVariable long openTime_id) {
-        OpenTime toReturn = openTimes.findById(openTime_id).orElseGet(() -> null);
-        return (toReturn == null ) ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
-                : new ResponseEntity<>(toReturn, HttpStatus.OK);
-    }
+//    /**
+//     * GET Endpoint to retrieve holiday by ID.
+//     *
+//     * @param openTime_id Unique identifier of the equipment.
+//     * @return The requested equipment {@link OpenTime}.
+//     */
+//
+//    @GetMapping(value = "/openTimes/{openTime_id}")
+//    public @ResponseBody ResponseEntity<OpenTime> getOpenTimeById(@PathVariable long openTime_id) {
+//        OpenTime toReturn = openTimes.findById(openTime_id).orElseGet(() -> null);
+//        return (toReturn == null ) ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+//                : new ResponseEntity<>(toReturn, HttpStatus.OK);
+//    }
 
     /**
      * POST Endpoint to add a new OpenTime.
