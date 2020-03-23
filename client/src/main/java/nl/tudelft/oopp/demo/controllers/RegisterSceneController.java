a582f22763181a1cd86cf7dc2192e1ebe944aba2
Atanas Pashov <a.i.pashov@student.tudelft.nl>
2020-03-23 01:02:59 +0100

    Connected the sign in scene with the UserCommunication class and now new users can create new accounts.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterSceneController.java
@@ -1,12 +1,24 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import nl.tudelft.oopp.demo.communication.UserCommunication;
+import nl.tudelft.oopp.demo.helperclasses.User;
 import nl.tudelft.oopp.demo.views.MainDisplay;
 
 public class RegisterSceneController implements Initializable {
+
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField password;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -15,6 +27,14 @@ public class RegisterSceneController implements Initializable {
     @FXML
     public void handleSignUpButton() {
         // have a sign up method on the server communication side
-        MainDisplay.registerStage.close();
+        User user = new User(email.getText(),"user", firstName.getText(), lastName.getText());
+        // TODO add a text field for username
+        if (UserCommunication.createNewUser(user, firstName.getText(), password.getText())){
+            MainDisplay.registerStage.close();
+        }
+        else {
+            // there is already a user with that username
+            return;
+        }
     }
 }
