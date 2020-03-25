c488c2ae471e0cc1e58a762ca40f861ff5461f77
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-03-25 22:08:43 +0100

    Improved checkstlye (import optimizations).

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Room.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Room.java
@@ -2,8 +2,21 @@ package nl.tudelft.oopp.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
-import java.util.*;
-import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import org.springframework.lang.Nullable;
 
 @Entity
