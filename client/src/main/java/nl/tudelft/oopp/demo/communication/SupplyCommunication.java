3f463ef413d2a5f406092ebe1f24cf42a1d514b3
Iulia Dinu <i.dinu-1@student.tudelft.nl>
2020-03-30 17:14:42 +0200

    Add update method for server communication Supply entity
    the update method now response with the response discussed in issue #create supplyCommunication.

--- client/src/main/java/nl/tudelft/oopp/demo/communication/SupplyCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/SupplyCommunication.java
@@ -1,6 +1,7 @@
 package nl.tudelft.oopp.demo.communication;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -123,5 +124,39 @@ public class SupplyCommunication {
             System.out.println("Status: " + response.statusCode());
         }
     }
+
+    /**
+     * Updates a supply.
+     *
+     * @throws Exception if communication with the server fails or if the response is not proper json.
+     */
+//Building building, String name, int stock
+    public static void updateSupply(long id, long buildingId, String name, int stock) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        Supply newSupply = new Supply(BuildingCommunication.getBuildingById(buildingId), name, stock);
+        String jsonSupply = "";
+        try {
+            jsonSupply = mapper.writeValueAsString(newSupply);
+            System.out.println(jsonSupply);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HttpRequest request = HttpRequest.newBuilder().header("Content-type", "application/json").PUT(HttpRequest.BodyPublishers.ofString(jsonSupply)).uri(URI.create(String.format("http://localhost:8080/supplies/%s", id))).setHeader("Cookie", Authenticator.SESSION_COOKIE).build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            //return "Communication with server failed";
+        }
+        if (response.statusCode() != 200) {
+            System.out.println("Status: " + response.statusCode());
+        }
+    }
+
 
 }
