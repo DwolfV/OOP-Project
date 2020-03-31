735d893c4bbce7335d6d316901b279394a75c632
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-03-31 19:32:25 +0200

    Added a simple skeleton for a friends scene.

--- client/src/main/java/nl/tudelft/oopp/demo/communication/Authenticator.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/Authenticator.java
@@ -4,7 +4,7 @@ public abstract class Authenticator {
     static String SESSION_COOKIE = "";
     static String ROLE = "";
     public static long ID = 0;
-    static String USERNAME = "";
+    public static String USERNAME = "";
 
     public static boolean isAdmin() {
         return ROLE.contains("ADMIN");
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/FriendsSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/FriendsSceneController.java
@@ -1,13 +1,30 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import nl.tudelft.oopp.demo.communication.Authenticator;
+import nl.tudelft.oopp.demo.communication.FriendCommunication;
+import nl.tudelft.oopp.demo.communication.RoomCommunication;
+import nl.tudelft.oopp.demo.communication.UserCommunication;
+import nl.tudelft.oopp.demo.helperclasses.Friend;
+import nl.tudelft.oopp.demo.helperclasses.Room;
+import nl.tudelft.oopp.demo.helperclasses.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FriendsSceneController implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
 
     /**
      * Called to initialize a controller after its root element has been
@@ -19,9 +36,20 @@ public class FriendsSceneController implements Initializable {
      */
     @Override
     public void initialize (URL location, ResourceBundle resources) {
-        VBox veBox = new VBox();
+
         HBox hoBox = new HBox();
+        Button deleteButton = new Button();
+        Button addButton = new Button();
+        hoBox.getChildren().addAll(deleteButton, addButton);
 
+        ListView<User> listView = new ListView<>();
+        ObservableList<User> friends = FXCollections.observableList(FriendCommunication.getFriends(Authenticator.USERNAME));
+        listView.setItems(friends);
 
+        VBox veBox = new VBox();
+        veBox.getChildren().addAll(listView, hoBox);
+
+        borderPane.setCenter(veBox);
+        borderPane.setPadding(new Insets(30, 5, 5, 10));
     }
 }
