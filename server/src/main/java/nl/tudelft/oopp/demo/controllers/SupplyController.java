3286bb9a947a50dee0a3e1001d24a00bbb54e100
Andrei <A.Mija@student.tudelft.nl>
2020-04-01 00:20:38 +0300

    Made supply controller a rest controller and fixed a bug in suppy repository

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/SupplyController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/SupplyController.java
@@ -8,18 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+@RestController
 public class SupplyController {
 
     @Autowired
@@ -30,7 +23,6 @@ public class SupplyController {
      * @return message
      */
     @GetMapping("/supply")
-    @ResponseBody
     public List<Supply> getAllSupplies() {
         return rep.findAll();
     }
@@ -56,7 +48,7 @@ public class SupplyController {
          */
 
     @GetMapping("/supply/building/{building_id}")
-    public @ResponseBody ResponseEntity<List<Supply>> getSupplyByBuildingId(@PathVariable(value = "building_id") long id) {
+    public ResponseEntity<List<Supply>> getSupplyByBuildingId(@PathVariable(value = "building_id") long id) {
         return rep.findByBuildingId(id).isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(rep.findByBuildingId(id), HttpStatus.OK);
     }
 
