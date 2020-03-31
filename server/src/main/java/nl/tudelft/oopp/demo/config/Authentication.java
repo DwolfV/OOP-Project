d0669b8420dc6b42905d1ae35572b9e85b24e038
Atanas Pashov <a.i.pashov@student.tudelft.nl>
2020-04-01 00:40:55 +0200

    Made usernames case sensitive.

--- server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/Authentication.java
@@ -7,9 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -24,8 +27,8 @@ public class Authentication extends WebSecurityConfigurerAdapter {
     // add two demo users
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
+        JdbcUserDetailsManagerConfigurer manager = auth.jdbcAuthentication()
+                .dataSource(dataSource).usersByUsernameQuery("select username, password, enabled from users where binary username = ?");
     }
 
 
