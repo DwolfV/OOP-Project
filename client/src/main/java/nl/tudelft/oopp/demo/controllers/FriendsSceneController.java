ae7915a1f7845731830732c72348eb2319b6c00f
Daniel Volpin <d.volpin@student.tudelft.nl>
2020-03-31 21:31:19 +0200

    Added controllers for the delete and add buttons on the friend scene.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/FriendsSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/FriendsSceneController.java
@@ -2,18 +2,19 @@ package nl.tudelft.oopp.demo.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import nl.tudelft.oopp.demo.communication.Authenticator;
-import nl.tudelft.oopp.demo.communication.FriendCommunication;
-import nl.tudelft.oopp.demo.communication.RoomCommunication;
-import nl.tudelft.oopp.demo.communication.UserCommunication;
+import nl.tudelft.oopp.demo.communication.*;
+import nl.tudelft.oopp.demo.helperclasses.Building;
 import nl.tudelft.oopp.demo.helperclasses.Friend;
 import nl.tudelft.oopp.demo.helperclasses.Room;
 import nl.tudelft.oopp.demo.helperclasses.User;
@@ -38,16 +39,46 @@ public class FriendsSceneController implements Initializable {
     public void initialize (URL location, ResourceBundle resources) {
 
         HBox hoBox = new HBox();
-        Button deleteButton = new Button();
-        Button addButton = new Button();
-        hoBox.getChildren().addAll(deleteButton, addButton);
+        Button deleteButton = new Button("Delete");
+        Button addButton = new Button("Add");
+        TextField newFriendTextField = new TextField();
+        newFriendTextField.setPromptText("username");
 
-        ListView<User> listView = new ListView<>();
+        hoBox.getChildren().addAll(deleteButton, addButton, newFriendTextField);
+
+        ListView<User> friendList = new ListView<>();
         ObservableList<User> friends = FXCollections.observableList(FriendCommunication.getFriends(Authenticator.USERNAME));
-        listView.setItems(friends);
+        friendList.setItems(friends);
+
+        deleteButton.setOnAction(event -> {
+            final int selectedIdx = friendList.getSelectionModel().getSelectedIndex();
+            if (selectedIdx != -1) {
+                User itemToRemove = friendList.getSelectionModel().getSelectedItem();
+
+                final int newSelectedIdx =
+                    (selectedIdx == friendList.getItems().size() - 1)
+                        ? selectedIdx - 1
+                        : selectedIdx;
+
+                friendList.getSelectionModel().select(newSelectedIdx);
+                //removes the player for the array
+                System.out.println("selectIdx: " + selectedIdx);
+                System.out.println("item: " + itemToRemove);
+                friends.remove(selectedIdx);
+
+            }
+        });
+
+        addButton.setOnAction(event -> {
+            User newFriendTextFieldText = (User) newFriendTextField.getText();
+
+            FriendCommunication.addFriendship(Authenticator.USERNAME, newFriendTextFieldText);
+
+            newFriendTextField.setText(null);
+        });
 
         VBox veBox = new VBox();
-        veBox.getChildren().addAll(listView, hoBox);
+        veBox.getChildren().addAll(friendList, hoBox);
 
         borderPane.setCenter(veBox);
         borderPane.setPadding(new Insets(30, 5, 5, 10));
