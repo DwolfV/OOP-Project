2a45a435230ba023612374232aad47c3695442f4
Bogdan <b.pietroianu@student.tudelft.nl>
2020-04-02 21:57:03 +0300

    Added an "Add to basket button" in the menu table

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MenuSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MenuSceneController.java
@@ -3,19 +3,19 @@ package nl.tudelft.oopp.demo.controllers;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
+import javafx.util.Callback;
 import javafx.util.converter.FloatStringConverter;
 import nl.tudelft.oopp.demo.communication.RestaurantDishCommunication;
 import nl.tudelft.oopp.demo.helperclasses.Dish;
@@ -24,6 +24,8 @@ public class MenuSceneController implements Initializable {
 
     public static TableView<Dish> tableView;
     private static Rectangle2D screenBounds;
+//    @FXML
+//    private Button Button;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -54,13 +56,46 @@ public class MenuSceneController implements Initializable {
             (TableColumn.CellEditEvent<Dish, String> t) -> t.getTableView().getItems().get(
                 t.getTablePosition().getRow()).setDescription(t.getNewValue()));
         // Price Column
-        TableColumn<Dish, Float> priceCol = new TableColumn<>("price");
+        TableColumn<Dish, Float> priceCol = new TableColumn<>("Price");
         priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
         priceCol.setCellFactory(TextFieldTableCell.forTableColumn(new FloatStringConverter()));
         priceCol.setOnEditCommit(
             (TableColumn.CellEditEvent<Dish, Float> t) -> t.getTableView().getItems().get(
                 t.getTablePosition().getRow()).setPrice(t.getNewValue()));
         priceCol.getStyleClass().add("price-col");
+        // Add button Column
+//        TableColumn addCol =
+//                new TableColumn<>();
+//        Button button1 = new Button("Menu");
+//        addCol.setCellValueFactory(button1);
+
+        TableColumn addCol = new TableColumn();
+        Callback<TableColumn<Dish, String>, TableCell<Dish, String>> cellFactory = (param) -> {
+            //Make tablecell including the button
+            final TableCell<Dish, String> cell = new TableCell<Dish, String>() {
+
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    }
+                    else {
+                        final Button addButton = new Button("Add to basket");
+                        addButton.setOnAction(event -> {
+                            Dish d = getTableView().getItems().get(getIndex());
+                            System.out.println("Your dish is: " + d.getId() + " " + d.getName());
+                        });
+                    setGraphic(addButton);
+                    setText(null);
+                    }
+                }
+            };
+            return cell;
+        };
+        addCol.setCellFactory(cellFactory);
+
 
         // Set styleClass to rows
         tableView.setRowFactory(tv -> {
@@ -71,7 +106,7 @@ public class MenuSceneController implements Initializable {
         );
 
         // Set Columns
-        tableView.getColumns().addAll(nameCol, descCol, priceCol);
+        tableView.getColumns().addAll(nameCol, descCol, priceCol, addCol);
         List<Dish> dishList = RestaurantDishCommunication.getDishesByRestaurant(restaurantId);
         ObservableList<Dish> dishObservableList = FXCollections.observableArrayList(dishList);
         tableView.setItems(dishObservableList);
